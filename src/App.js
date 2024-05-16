2dd0bcc20eb288e40fdba3aad5bb3d67c56e1375
SapiTello <saidapitello.12@gmail.com>
2024-05-16 00:27:03 -0500

    feat[ZEGEL]: Estilos con React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Session 2 - Zegel (Hola Mundo)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='form_container'>
+      <div className='form_header'>
+        <h1 className='form_title'>
+          Mi Primer formulario
+        </h1>
+
+      </div>
+
+    </div>    
   );
 }
 
