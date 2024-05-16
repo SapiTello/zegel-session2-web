12dbcd39eab058153e62e7ecc19fdefdd4daf1eb
SapiTello <saidapi@hotmail.com>
2024-05-16 17:19:21 -0500

    add:  Navegabilidad

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className='form_container'>
-      <div className='form_header'>
-        <h1 className='form_title'>
-          Mi Primer formulario
-        </h1>
-
-      </div>
-
-    </div>    
+    <div className='form_container'>      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      </BrowserRouter>        
+    </div>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,23 @@
+import '../App.css';
+
+function Nav() {
+  return (
+    <div className='form_container_contact'>    
+      <div className='form_header'>
+        <h1 className='form_title'>
+          Acerca de
+        </h1>
+      </div>
+      <div className='form_body'>
+        <div className='form'>
+          <label className='form_label'>
+          Simplificar y disfrutar más la vida es el proposito con el que trabajamos para quienes nos eligen todos los días. Esta visión
+          comun esta en el centro de nuestra conversaciones y guia nuestros poryectos de transformacion cultura y digital.
+          </label>          
+        </div>        
+      </div>
+    </div>    
+  );
+}
+
+export default Nav;
